c22e46eddd6133a1394ceada5fe8336c0d4c47c2
Tommaso Venza <tommasovenza@gmail.com>
2020-09-09 18:34:52 +0200

    compilata show

--- resources/views/record/show.blade.php
+++ resources/views/record/show.blade.php
@@ -1,1 +1,14 @@
-<h1>ciao sono show</h1>
+<h1>Titolo Disco: {{ $record->titolo }} </h1>
+
+<ul>
+    
+    <li>{{ $record->artista }}</li>
+    <li>{{ $record->anno }}</li>
+    <li>{{ $record->numero_brani }}</li>
+    <li>{{ $record->descrizione }}</li>
+   
+</ul>
+
+
+
+<a href="{{ route('record.index')}}">Torna alla home</a>
