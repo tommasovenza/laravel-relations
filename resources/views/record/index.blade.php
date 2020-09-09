61815fcd48c72e4971f19afd52c35e9179089fdc
Tommaso Venza <tommasovenza@gmail.com>
2020-09-09 18:13:24 +0200

    stampato lista dischi presenti nel database nella view (record.index)

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -1,1 +1,17 @@
-<h1>ciao sono la view</h1>
+<h1>Lista dischi presenti in archivio</h1>
+
+<ul>
+
+    @foreach ($records as $record)
+
+    <li> Titolo disco: {{ $record->titolo }}</li>
+    <li> Nome artista: {{$record->artista}} </li>
+    <li> Anno disco: {{$record->anno}} </li>
+    <li> Numero brani presenti {{$record->numero_brani}}</li>
+    <li> Descrizione: {{$record->descrizione}}</li>
+
+    <br>
+
+    @endforeach
+
+</ul>
